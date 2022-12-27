3231da0f72b2bf18ba4708890f3e9c1e769e41c3
unknown <saiharshithpalepu@gmail.com>
2022-12-27 16:21:25 -0500

    'a'

---
+++ src/Components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+// function Greet(){
+//     return <h1>Hello harshith</h1>
+// }
+
+const Greet = ()=>{
+    return <h1>Hello Harshith</h1>
+}
+
+export default Greet
